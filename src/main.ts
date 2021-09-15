6731728544bdec09a62b2a55742262e9c2701e35
v_zihzhong <v_zihzhong@tencent.com>
2021-09-15 20:04:59 +0800

    feat: 用户接口初始化

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('彩虹海🌈接口API')
+    .setVersion('1.0.0')
+    .addTag('Rainbow-Sea')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('apidoc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
