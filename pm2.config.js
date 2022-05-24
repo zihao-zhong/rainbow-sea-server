92f20adf110e0be87420a4e502c3ef37a722d240
v_zihzhong <v_zihzhong@tencent.com>
2022-05-24 09:29:59 +0800

    feat: 增加pm2配置文件

--- pm2.config.js
+++ pm2.config.js
@@ -3,6 +3,10 @@ module.exports = {
     {
       name: 'rainbow-server',
       script: './dist/main.js',
+      // exec_mode: 'cluster', // 启动集群模式
+      // instances: 'max', // 启动进程数量，可开启多进程
+      max_memory_restart: '300M', // 内存达到阈值之后重启应用
+      exp_backoff_restart_delay: 100, // 指数退避重启延迟
       env: {
         NODE_ENV: 'development',
       },
